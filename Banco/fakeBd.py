eedb64a97deb60cd79ae55cfaeac8820d707ca7f
JaderMac <jadermachado@live.com>
2026-04-25 14:24:30 -0300

    Adicionando arquivo banco fake para testes

---
+++ Banco/fakeBd.py
@@ -0,0 +1,91 @@
+Usuarios = [ 
+    {
+        'id': 1,
+        'nome':'Bianca',
+        'senha': 112233,
+        'email': 'bianca@univesp.com'
+    },
+    {
+        'id': 2,
+        'nome':'Brener',
+        'senha': 112233,
+        'email': 'brener@univesp.com'
+    },
+    {
+        'id': 3,
+        'nome':'Edson',
+        'senha': 112233,
+        'email': 'edson@univesp.com'
+    },
+    {
+        'id': 4,
+        'nome':'Luiz Felipe',
+        'senha': 112233,
+        'email': 'luizfelipe@univesp.com'
+    },
+    {
+        'id': 5,
+        'nome':'Kimberly',
+        'senha': 112233,
+        'email': 'kimberly@univesp.com'
+    },
+    {
+        'id': 6,
+        'nome':'Jader',
+        'senha': 112233,
+        'email': 'jader@univesp.com'
+    },
+    {
+        'id': 7,
+        'nome':'Jessica',
+        'senha': 112233,
+        'email': 'jessica@univesp.com'
+    }
+]
+
+clientes = [
+    {
+        'id': 1,
+        'nome':'Jader Cliente',
+        'celular': "(12)98888-8888",
+        'email': 'jader@apple.br'
+    },
+    {
+        'id': 2,
+        'nome':'Luiz Cliente',
+        'celular': "(12)99999-9999",
+        'email': 'luiz@gov.br'
+    }
+]
+
+locacoes = [
+    {
+        'id': 1,
+        'nome':'Quarto VIP em Campos',
+        'descricao': "Quarto com varanda, banheiro amplo com hidromassagem, sala com lareira.",
+        'valordiaria': 1000
+    },
+    {
+        'id': 2,
+        'nome':'Quarto VIP em Campos + serviço premium',
+        'descricao': "Quarto com varanda, banheiro amplo com hidromassagem, sala com lareira, cocierge disponivel",
+        'valordiaria': 2000
+    }
+]
+
+estadias = [
+    {   
+        "id":1,
+        "locação_id":1,
+        "cliente_id":1,
+        "data_inicial":"17/04/2026",
+        "data_final": "21/04/2026"
+    },
+    {
+        "id":2,
+        "locação_id":2,
+        "cliente_id":2,
+        "data_inicial":"17/04/2026",
+        "data_final": "21/04/2026"           
+    }
+]
